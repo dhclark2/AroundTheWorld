a08b60498008d4dae937e7d6358f04cdc46c2776
Michael Grein <michaelgrein92@gmail.com>
2017-01-05 10:42:48 -0500

    Made price and cuisine type options as a drop-down

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,14 @@
 class Restaurant < ApplicationRecord
+  CUISINES = [ "", "Italian", "Burgers" ]
+  PRICES = [
+    ["", 0],
+    ["$", 1],
+    ["$$", 2],
+    ["$$$", 3],
+    ["$$$$", 4],
+    ["$$$$$", 5]
+  ]
+
   validates :address, presence: true
   validates :hours, presence: true
   validates :name, presence: true
