ada48f6819994fe141a5e7878e19d2192cc62767
Kenia Valladarez <keniavalladarez@gmail.com>
2017-01-05 11:10:43 -0500

    Added login/logout paths in the navbar

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   get    '/auth/:provider'          => 'omniauth#auth',    as: :auth
   get    '/auth/:provider/callback' => 'session#create'
   get    '/auth/failure'            => 'session#failure'
+
+  get '/login' => 'session#new'
+  post '/login' => 'session#create'
+  get '/logout' => 'session#destroy'
 end
