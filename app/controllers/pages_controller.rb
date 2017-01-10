452b7954c671c35fdfae52907523b4a82edabaee
Gavin Stark <gavin@gstark.com>
2017-01-10 11:45:42 -0500

    Landing page improvement
    Has some dynamic data with restaurant listings and cuisine search

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,7 @@ class PagesController < ApplicationController
   layout false
 
   def landing
+    # @latest_restaurants = Restaurant.order(created_at: :desc).limit(5)
+    @latest_restaurants = Restaurant.all
   end
 end
