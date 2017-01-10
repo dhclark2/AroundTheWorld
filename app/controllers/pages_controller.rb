265b8eacbf00e8a6680aa839b39268c0460a3c21
Gavin Stark <gavin@gstark.com>
2017-01-10 12:19:20 -0500

    Integrates template into application and use it on individual pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,4 @@
 class PagesController < ApplicationController
-  layout false
-
   def landing
     # @latest_restaurants = Restaurant.order(created_at: :desc).limit(5)
     @latest_restaurants = Restaurant.all
